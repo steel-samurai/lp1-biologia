2eac06b782b0a02ccf563d5b144192fe27a9b35a
izabel kaitel <fishsfear@gmail.com>
2023-05-06 21:16:21 -0300

    basicao das pags interativas

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Set the width of the sidebar to 250px (show it) */
+function abreTabela() {
+    document.getElementById("abrivel").style.height = "200px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function fechaTabela() {
+    document.getElementById("abrivel").style.height = "40px";
+  }
