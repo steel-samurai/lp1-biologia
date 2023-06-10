e96fb03200044be3f9156b63449df1587dbdd14c
Luis Gustavo <luisgpl1508@gmail.com>
2023-06-10 09:59:42 -0300

    aaa

--- script.js
+++
@@ -1,7 +0,0 @@
-function abreTabela() {
-    document.getElementById("abrivel").style.height = "200px";
-  }
-  
-  function fechaTabela() {
-    document.getElementById("abrivel").style.height = "40px";
-  }
